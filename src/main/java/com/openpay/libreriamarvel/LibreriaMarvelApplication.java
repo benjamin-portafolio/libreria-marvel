8e47bee8a4ed310998de401189206f14140c7ddd
Benjamin Mac <benjamin94833@gmail.com>
2024-03-24 18:31:42 -0600

    se realiza commit inicial

---
+++ src/main/java/com/openpay/libreriamarvel/LibreriaMarvelApplication.java
@@ -0,0 +1,13 @@
+package com.openpay.libreriamarvel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibreriaMarvelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LibreriaMarvelApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/openpay/libreriamarvel/LibreriaMarvelApplicationTests.java
@@ -0,0 +1,13 @@
+package com.openpay.libreriamarvel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibreriaMarvelApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
